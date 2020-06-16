f6ff19a22f855bba277cde67032ed017a46ee7e4
WollyOT <smith.paul.1232@gmail.com>
2020-06-16 17:05:51 -0400

    Added state change for navbar

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor() {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link' >
+            <button 
+              className='btn-clear nav-link'
+              onClick={() => this.updateLanguage(language)}>
               {language} 
             </button>
           </li>
