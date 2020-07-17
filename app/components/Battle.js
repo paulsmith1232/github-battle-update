60ba1caa9a1034bc1660b73703c2edbb5347ee09
WollyOT <smith.paul.1232@gmail.com>
2020-07-17 17:21:40 -0400

    Updated Battle component to use query strings

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -3,6 +3,7 @@ import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icon
 import PropTypes from 'prop-types'
 import Results from './Results'
 import { ThemeConsumer } from '../contexts/theme'
+import { Link } from 'react-router-dom'
 
 function Instructions () {
   return (
@@ -134,8 +135,7 @@ export default class Battle extends React.Component {
     
     this.state = {
       playerOne: null,
-      playerTwo: null,
-      battle: false
+      playerTwo: null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)    
@@ -154,18 +154,6 @@ export default class Battle extends React.Component {
 
   render() {
     const { playerOne, playerTwo, battle } = this.state
-
-    if(battle===true) {
-      return <Results 
-        playerOne={playerOne} 
-        playerTwo={playerTwo} 
-        onReset={() => this.setState({
-          playerOne: null,
-          playerTwo: null,
-          battle: false
-        })}  
-      />
-    }
 
     return (
       <React.Fragment>
@@ -200,12 +188,15 @@ export default class Battle extends React.Component {
           </div>
 
           {playerOne && playerTwo && (
-            <button 
+            <Link 
               className='btn dark-btn btn-space'
-              onClick={() => this.setState({battle: true}) }
+              to={{
+                pathname: '/battle/results',
+                search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+              }}
             >
               Battle
-            </button>
+            </Link>
           )}
         </div>
         
